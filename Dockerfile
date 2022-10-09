e85790d366742bf1451a0f7edd17c16eebdb1d78
brionnevi <victor.brionne@gmail.com>
2022-10-09 20:44:23 +0200

    Added build step to Dockerfile Modified SecurityConfig

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
+FROM maven:3.8.6-openjdk-11 as build
+COPY src usr/src/api/src
+COPY pom.xml usr/src/api
+RUN mvn -f usr/src/api/pom.xml clean package
+
 FROM openjdk:11
-EXPOSE 8080
+EXPOSE 9000
 WORKDIR .
-COPY target/library_api-0.0.1-SNAPSHOT.jar .
-ENTRYPOINT ["java", "-jar", "library_api-0.0.1-SNAPSHOT.jar"]
+COPY --from=build /usr/src/api/target/library_api-0.0.1-SNAPSHOT.jar /usr/api/library_api-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java", "-jar", "/usr/api/library_api-0.0.1-SNAPSHOT.jar"]
--- src/main/java/fr/ensicaen/ensibrary/libraryapi/configuration/SecurityConfig.java
+++ src/main/java/fr/ensicaen/ensibrary/libraryapi/configuration/SecurityConfig.java
@@ -2,12 +2,16 @@ package fr.ensicaen.ensibrary.libraryapi.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true
+)
 @EnableMethodSecurity
 public class SecurityConfig extends GlobalMethodSecurityConfiguration {
 
