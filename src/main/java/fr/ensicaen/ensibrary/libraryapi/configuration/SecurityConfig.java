93175c22f512e6fd462de71c32eb3e7d3a9d7aa2
brionnevi <victor.brionne@gmail.com>
2022-10-11 20:18:07 +0200

    Updated CORS config

--- src/main/java/fr/ensicaen/ensibrary/libraryapi/configuration/SecurityConfig.java
+++ src/main/java/fr/ensicaen/ensibrary/libraryapi/configuration/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -21,5 +23,15 @@ public class SecurityConfig extends GlobalMethodSecurityConfiguration {
                 authorizeRequests.anyRequest().authenticated()
         ).cors().and().csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
